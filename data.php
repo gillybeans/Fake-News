a6c7874dfa103d9b7f7e1214a7abf013fd27db0f
Gilda A <gilda.ahmadniaye@gmail.com>
2020-10-19 12:45:19 +0200

    started coding

---
+++ data.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+$posts = [
+[ 
+    'title' => 'Cows lose their jobs as milk prices drop', 
+    'author' => 'Moo Moo', 
+    'content' => 'Doggo ipsum heckin good boys and girls long water shoob h*ck you are doin me a concern wow very biscit, super chub you are doing me a frighten thicc. Vvv shibe heckin good boys boof noodle horse ruff, blop puggorino thicc doge. mlem shooberino doggorino. you are doing me a frighten lotsa pats fat boi. Blop doing me a frighten blop I am bekom fat porgo boof, smol borking doggo with a long snoot for pats doggo waggy wags such treat.
+
+    Mlem borkdrive smol borking doggo with a long snoot for pats noodle horse, vvv sub woofer. Doge big ol long woofer the neighborhood pupper yapper shooberino, shoober dat tungg tho very jealous pupper you are doing me a frighten. borking doggo most angery pupper I have ever seen doggo. H*ck I am bekom fat long doggo very hand that feed shibe pupperino, long water shoob corgo heck. Blep clouds maximum borkdrive, lotsa pats. Borkf ur givin me a spook maximum borkdrive mlem smol borking doggo with a long snoot for pats adorable doggo long bois, I am bekom fat what a nice floof tungg long woofer heckin good boys and girls. Porgo doing me a frighten shoober wow very biscit doge long woofer, doing me a frighten porgo smol shoober. Long water shoob noodle horse pats very hand that feed shibe pupper snoot, puggorino such treat big ol.', 
+    'published_date' => '2020-02-03', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'World Bank says poor need more money', 
+    'author' => 'Im Richmann', 
+    'content' => 'Doggo ipsum you are doin me a concern waggy wags porgo h*ck doggo, what a nice floof wow very biscit such treat. Shoob noodle horse corgo tungg maximum borkdrive, heck borkdrive porgo, shooberino what a nice floof long bois. Ur givin me a spook what a nice floof woofer, boof. Long water shoob pats heckin good boys and girls adorable doggo such treat doing me a frighten, very taste wow pupper sub woofer. I am bekom fat borkdrive long doggo borkf dat tungg tho noodle horse very jealous pupper extremely cuuuuuute stop it fren, boof clouds very hand that feed shibe you are doing me a frighten very taste wow aqua doggo. Snoot big ol pupper boofers such treat what a nice floof borkdrive, pats shoob shoober. Heck adorable doggo doge smol borking doggo with a long snoot for pats, boof wrinkler. He made many woofs mlem heckin angery woofer, puggo. You are doin me a concern fat boi doggorino stop it fren boof, adorable doggo aqua doggo you are doin me a concern.
+
+    Heckin angery woofer clouds porgo mlem stop it fren, shoob maximum borkdrive such treat. Aqua doggo borkf fat boi, pupperino.  Pupperino extremely cuuuuuute mlem you are doin me a concern big ol pupper he made many woofs, thicc borking doggo smol long water shoob, pupper mlem long woofer I am bekom fat. Shoober clouds doing me a frighten doge, you are doing me the shock. Fat boi very taste wow you are doin me a concern much ruin diet, you are doin me a concern long woofer. Big ol pupper wrinkler you are doing me a frighten fat boi, length boy. Long woofer dat tungg tho big ol thicc dat tungg tho I am bekom fat, most angery pupper I have ever seen snoot big ol boofers length boy, big ol wow such tempt waggy wags doggo.  Big ol pupper much ruin diet noodle horse vvv blop, clouds porgo.', 
+    'published_date' => '2020-02-09', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Murderer says detective ruined his reputation', 
+    'author' => 'Sherlock Holmes', 
+    'content' => 'Doggo ipsum yapper heck. Adorable doggo length boy ur givin me a spook snoot, doggorino. Tungg shoob borkdrive maximum borkdrive such treat very jealous pupper, very taste wow yapper shooberino borkf. The neighborhood pupper stop it fren doggo boofers big ol, blop very good spot adorable doggo. Borkf shoob corgo wow such tempt he made many woofs, thicc heckin good boys and girls most angery pupper I have ever seen. Stop it fren you are doin me a concern heckin sub woofer he made many woofs snoot shibe, adorable doggo shooberino such treat heckin angery woofer adorable doggo. Yapper blep sub woofer heckin good boys and girls, waggy wags.', 
+    'published_date' => '2020-02-17', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'State population to double by 2040; babies to blame', 
+    'author' => 'Mister Babyhater', 
+    'content' => 'Doggo ipsum yapper heck. Adorable doggo length boy ur givin me a spook snoot, doggorino. Tungg shoob borkdrive maximum borkdrive such treat very jealous pupper, very taste wow yapper shooberino borkf. The neighborhood pupper stop it fren doggo boofers big ol, blop very good spot adorable doggo. Borkf shoob corgo wow such tempt he made many woofs, thicc heckin good boys and girls most angery pupper I have ever seen. Stop it fren you are doin me a concern heckin sub woofer he made many woofs snoot shibe, adorable doggo shooberino such treat heckin angery woofer adorable doggo. Yapper blep sub woofer heckin good boys and girls, waggy wags.', 
+    'published_date' => '2020-02-17', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Federal Agents raid gun shop - find weapons', 
+    'author' => 'Dick Gaysson', 
+    'content' => 'Doggo ipsum you are doin me a concern waggy wags porgo h*ck doggo, what a nice floof wow very biscit such treat. Shoob noodle horse corgo tungg maximum borkdrive, heck borkdrive porgo, shooberino what a nice floof long bois. Ur givin me a spook what a nice floof woofer, boof. Long water shoob pats heckin good boys and girls adorable doggo such treat doing me a frighten, very taste wow pupper sub woofer. I am bekom fat borkdrive long doggo borkf dat tungg tho noodle horse very jealous pupper extremely cuuuuuute stop it fren, boof clouds very hand that feed shibe you are doing me a frighten very taste wow aqua doggo. Snoot big ol pupper boofers such treat what a nice floof borkdrive, pats shoob shoober. Heck adorable doggo doge smol borking doggo with a long snoot for pats, boof wrinkler. He made many woofs mlem heckin angery woofer, puggo. You are doin me a concern fat boi doggorino stop it fren boof, adorable doggo aqua doggo you are doin me a concern.
+
+    Heckin angery woofer clouds porgo mlem stop it fren, shoob maximum borkdrive such treat. Aqua doggo borkf fat boi, pupperino.  Pupperino extremely cuuuuuute mlem you are doin me a concern big ol pupper he made many woofs, thicc borking doggo smol long water shoob, pupper mlem long woofer I am bekom fat. Shoober clouds doing me a frighten doge, you are doing me the shock. Fat boi very taste wow you are doin me a concern much ruin diet, you are doin me a concern long woofer. Big ol pupper wrinkler you are doing me a frighten fat boi, length boy. Long woofer dat tungg tho big ol thicc dat tungg tho I am bekom fat, most angery pupper I have ever seen snoot big ol boofers length boy, big ol wow such tempt waggy wags doggo.  Big ol pupper much ruin diet noodle horse vvv blop, clouds porgo.', 
+    'published_date' => '2020-02-09', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Rooms with broken air conditioners are hot', 
+    'author' => '', 
+    'content' => 'Doggo ipsum heckin good boys and girls long water shoob h*ck you are doin me a concern wow very biscit, super chub you are doing me a frighten thicc. Vvv shibe heckin good boys boof noodle horse ruff, blop puggorino thicc doge. mlem shooberino doggorino. you are doing me a frighten lotsa pats fat boi. Blop doing me a frighten blop I am bekom fat porgo boof, smol borking doggo with a long snoot for pats doggo waggy wags such treat.
+
+    Mlem borkdrive smol borking doggo with a long snoot for pats noodle horse, vvv sub woofer. Doge big ol long woofer the neighborhood pupper yapper shooberino, shoober dat tungg tho very jealous pupper you are doing me a frighten. borking doggo most angery pupper I have ever seen doggo. H*ck I am bekom fat long doggo very hand that feed shibe pupperino, long water shoob corgo heck. Blep clouds maximum borkdrive, lotsa pats. Borkf ur givin me a spook maximum borkdrive mlem smol borking doggo with a long snoot for pats adorable doggo long bois, I am bekom fat what a nice floof tungg long woofer heckin good boys and girls. Porgo doing me a frighten shoober wow very biscit doge long woofer, doing me a frighten porgo smol shoober. Long water shoob noodle horse pats very hand that feed shibe pupper snoot, puggorino such treat big ol.', 
+    'published_date' => '2020-02-03', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Planes are forced to land at airports', 
+    'author' => 'Jay Jay the Jet Plane', 
+    'content' => 'Doggo ipsum heckin good boys and girls long water shoob h*ck you are doin me a concern wow very biscit, super chub you are doing me a frighten thicc. Vvv shibe heckin good boys boof noodle horse ruff, blop puggorino thicc doge. mlem shooberino doggorino. you are doing me a frighten lotsa pats fat boi. Blop doing me a frighten blop I am bekom fat porgo boof, smol borking doggo with a long snoot for pats doggo waggy wags such treat.
+
+    Mlem borkdrive smol borking doggo with a long snoot for pats noodle horse, vvv sub woofer. Doge big ol long woofer the neighborhood pupper yapper shooberino, shoober dat tungg tho very jealous pupper you are doing me a frighten. borking doggo most angery pupper I have ever seen doggo. H*ck I am bekom fat long doggo very hand that feed shibe pupperino, long water shoob corgo heck. Blep clouds maximum borkdrive, lotsa pats. Borkf ur givin me a spook maximum borkdrive mlem smol borking doggo with a long snoot for pats adorable doggo long bois, I am bekom fat what a nice floof tungg long woofer heckin good boys and girls. Porgo doing me a frighten shoober wow very biscit doge long woofer, doing me a frighten porgo smol shoober. Long water shoob noodle horse pats very hand that feed shibe pupper snoot, puggorino such treat big ol.', 
+    'published_date' => '2020-02-03', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Woman missing since she was lost', 
+    'author' => 'Maybe Detective', 
+    'content' => 'Doggo ipsum you are doin me a concern waggy wags porgo h*ck doggo, what a nice floof wow very biscit such treat. Shoob noodle horse corgo tungg maximum borkdrive, heck borkdrive porgo, shooberino what a nice floof long bois. Ur givin me a spook what a nice floof woofer, boof. Long water shoob pats heckin good boys and girls adorable doggo such treat doing me a frighten, very taste wow pupper sub woofer. I am bekom fat borkdrive long doggo borkf dat tungg tho noodle horse very jealous pupper extremely cuuuuuute stop it fren, boof clouds very hand that feed shibe you are doing me a frighten very taste wow aqua doggo. Snoot big ol pupper boofers such treat what a nice floof borkdrive, pats shoob shoober. Heck adorable doggo doge smol borking doggo with a long snoot for pats, boof wrinkler. He made many woofs mlem heckin angery woofer, puggo. You are doin me a concern fat boi doggorino stop it fren boof, adorable doggo aqua doggo you are doin me a concern.
+
+    Heckin angery woofer clouds porgo mlem stop it fren, shoob maximum borkdrive such treat. Aqua doggo borkf fat boi, pupperino.  Pupperino extremely cuuuuuute mlem you are doin me a concern big ol pupper he made many woofs, thicc borking doggo smol long water shoob, pupper mlem long woofer I am bekom fat. Shoober clouds doing me a frighten doge, you are doing me the shock. Fat boi very taste wow you are doin me a concern much ruin diet, you are doin me a concern long woofer. Big ol pupper wrinkler you are doing me a frighten fat boi, length boy. Long woofer dat tungg tho big ol thicc dat tungg tho I am bekom fat, most angery pupper I have ever seen snoot big ol boofers length boy, big ol wow such tempt waggy wags doggo.  Big ol pupper much ruin diet noodle horse vvv blop, clouds porgo.', 
+    'published_date' => '2020-02-09', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+[ 
+    'title' => 'Diana was still alive hours before she died', 
+    'author' => 'Fake News', 
+    'content' => 'Doggo ipsum yapper heck. Adorable doggo length boy ur givin me a spook snoot, doggorino. Tungg shoob borkdrive maximum borkdrive such treat very jealous pupper, very taste wow yapper shooberino borkf. The neighborhood pupper stop it fren doggo boofers big ol, blop very good spot adorable doggo. Borkf shoob corgo wow such tempt he made many woofs, thicc heckin good boys and girls most angery pupper I have ever seen. Stop it fren you are doin me a concern heckin sub woofer he made many woofs snoot shibe, adorable doggo shooberino such treat heckin angery woofer adorable doggo. Yapper blep sub woofer heckin good boys and girls, waggy wags.', 
+    'published_date' => '2020-02-17', 
+    'likes' => '',
+    'img' => 'https://picsum.photos/650/300',
+],
+];
